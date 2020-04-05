4f62429704f89e9d856de2717570e9439582f889
Busari Habib <34791977+Bahat159@users.noreply.github.com>
2020-04-05 22:38:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+
+class PopUpInfo extends HTMLElement {
+   constructor() {
+          super();
+
+ const shadow = this.attachShadow({mode: 'open'});
+ const wrapper = documet.createElement('span');
+ wrapper.setAttribute('class', 'wrapper');
+
+ const icon = document.createElement('span');
+ icon.setAttribute('class', 'icon');
+ icon.setAttribute('tabindex', 0);
+
+ const info = document.createElement('span');
+ info.setAttribute('class', 'info');
+
+ const text = this.getAttribute('data-text');
+ info.textContent = text;
+
+ let imgUrl;
+ if(this.hasAttribute('img)) {
+    imgUrl = this.getAttribute('img');
+   } else {
+     imgUrl = '/img/default.png';
+    }
+ const img = document.createElement('img');
+ img.src = imgUrl;
+ icon.appendChild(img);
+
+ const style = document.createElement('style');
+ console.log(style.isConnected);
+
+ style.textContent = `
+      .wrapper {
+        position: relative;
+      }
+      .info {
+        font-size: 0.8rem;
+        width: 200px;
+        display: inline-block;
+        border: 1px solid black;
+        padding: 10px;
+        background: white;
+        border-radius: 10px;
+        opacity: 0;
+        transition: 0.6s all;
+        position: absolute;
+        bottom: 20px;
+        left: 10px;
+        z-index: 3;
+      }
+      img {
+        width: 1.2rem;
+      }
+      .icon:hover + .info, .icon:focus + .info {
+        opacity: 1;
+      }
+    `;
+
+ shadow.appendChild(style);
+ console.log(style.isConnected);
+ shadow.appendChild(wrapper);
+ wrapper.appendChild(icon);
+ wrapper.appendChild(info);
+     }
+  }
+ customElement.define('popup-info', PopUpInfo);
+
+
