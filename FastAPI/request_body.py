0b6d84844a1a4a800cab0e25d7085c20f34c76b3
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-17 05:27:38 -0600

    Update request_body.py

--- FastAPI/request_body.py
+++ FastAPI/request_body.py
@@ -17,3 +17,50 @@ app = FastAPI()
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# http://127.0.0.1:8000/request_and_path_parameter/678
+# {
+#  "item_id": 678,
+#  "name": "string",
+#  "description": "string",
+#  "price": 0,
+#  "tax": 0
+# }
+# http://127.0.0.1:8000/request_and_path_parameter/345
+# {
+#  "request_and_path_parameter": 345,
+#  "name": "string",
+#  "description": "string",
+#  "price": 0,
+#  "tax": 0
+# }
+
+@app.put("/request_and_path_parameter/{request_and_path_parameter}")
+async def create_request_and_path_parameter_item(request_and_path_parameter: int, item: Item):
+    return {"request_and_path_parameter": request_and_path_parameter, **item.dict()}
+
+# Request body + path + query parameters
+# http://127.0.0.1:8000/request_and_path_and_query_parameter/123?query=justinTime
+# {
+#  "request_and_path_and_query_parameter": 123,
+#  "name": "string",
+#  "description": "string",
+#  "price": 0,
+#  "tax": 0,
+#  "query": "justinTime"
+# }
+
+@app.put("/request_and_path_and_query_parameter/{request_and_path_and_query_parameter}")
+async def create_request_and_path_and_query_parameter_item(request_and_path_and_query_parameter: int, item: Item, query: Optional[str] = None):
+    result = {"request_and_path_and_query_parameter": request_and_path_and_query_parameter, **item.dict()}
+    if query:
+        result.update({"query": query})
+    return result
