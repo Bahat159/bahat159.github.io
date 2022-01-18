8cd40c27fef34063166dd26ee5804c2ab62f3e01
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-18 03:56:44 -0600

    Update url_model_body.py

--- FastAPI/main/url_model_body.py
+++ FastAPI/main/url_model_body.py
@@ -11,6 +11,10 @@ class Item(BaseModel):
     price: float
     tax: Optional[float] = None
 
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
 
 # http://127.0.0.1:8000/path_query_and_body_parameter/677?query=JustInTime
 # {"item_id": 677,"query": "JustInTime","item": {"name": "string","description": "string","price": 0,"tax": 0}}
@@ -25,4 +29,28 @@ async def update_path_query_and_body_parameter_item(*,item_id: int = Path(..., t
         results.update({"query": query})
     if item:
         results.update({"model_body_item": item})
+    return results
+
+
+def validate_data_type(data, type):
+    return isinstance(data, type)
+
+# Multiple body parameters
+# curl -X 'PUT' \'http://127.0.0.1:8000/multiple_body_parameter/7878' \-H 'accept: application/json' \-H 'Content-Type: application/json' \-d '{"item": {"name": "string","description": "string","price": 0,"tax": 0},"user": {"username": "string","full_name": "string"}}'
+# 
+# http://127.0.0.1:8000/multiple_body_parameter/7878
+# {"user_fullname": "string","user_username": "string","item_name": "string","item_description": "string","item_price": 0,# "item_tax": 0}
+#
+# http://127.0.0.1:8000/multiple_body_parameter/6767
+# [{"user.full_name": "Busari Habibullaah"},{"user.username": "Bahat"},{"item.name": "new_path"},{"item.description":
+#  "Another FastApi tutorial"},{"item.price": "$0.77"},{"item.tax": "$0.002"}]
+
+@app.put("/multiple_body_parameter/{item_id}")
+async def update_multiple_body_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item,"user": user}
+    item_id_check = validate_data_type(item_id, int)
+    if item_id_check:
+        return [{"user.full_name": "Busari Habibullaah"}, {"user.username": "Bahat"}, {"item.name": "new_path"}, {"item.description":"Another FastApi tutorial"} , {"item.price": "$0.77"}, {"item.tax":"$0.002"}]
+    else:
+        return ("Data type not of Integer")
     return results
