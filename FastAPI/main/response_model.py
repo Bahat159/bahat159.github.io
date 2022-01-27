9a6152b6defd03238e03b71f333d2d70e22703f3
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-27 11:58:58 -0600

    Update response_model.py

--- FastAPI/main/response_model.py
+++ FastAPI/main/response_model.py
@@ -17,3 +17,16 @@ class ResponseModelItem(BaseModel):
 @app.post("/create_response_model_item/", response_model=ResponseModelItem)
 async def create_response_model_item(item: ResponseModelItem):
     return item
+
+# Return the same input data
+
+class UserIn(BaseModel):
+    username: str = "Bahat"
+    password: str = "Ju99sterpo00se%\intT0oF5astA0pi" # we could be sending our user's passwords to every client.
+    email: str = "busarihabib159@gmail.com"
+    full_name: Optional[str] = "Busari Habibullaah"
+
+# Don't do this in production!
+@app.post("/create_user_input/", response_model=UserIn)
+async def create_user_input(user: UserIn):
+    return user
