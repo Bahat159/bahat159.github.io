4222e130dcf4feea1d194845dc1b19dee54adb95
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-27 12:16:26 -0600

    Update response_model.py

--- FastAPI/main/response_model.py
+++ FastAPI/main/response_model.py
@@ -39,3 +39,25 @@ class UserOut(BaseModel):
 @app.post("/user_output_model/", response_model=UserOut)
 async def create_user_output_model(user: UserIn):
     return user
+
+
+# Response Model encoding parameters
+
+class ResponseModelEncodingParameterItem(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: float = 10.5
+    tags: List[str] = []
+    
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/response_model_encoding_parameter_items/{item_id}", response_model=ResponseModelEncodingParameterItem, response_model_exclude_unset=True)
+async def read_response_model_encoding_parameter_item(item_id: str):
+    for x in items:
+        return items[x]
