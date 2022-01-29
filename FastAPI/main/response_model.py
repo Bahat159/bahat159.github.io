3b92f010f7c8680df7f9f96bbde806c0a97ef7e0
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-29 00:53:24 -0600

    Update response_model.py

--- FastAPI/main/response_model.py
+++ FastAPI/main/response_model.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, status, Form, UploadFile, File, HTTPException
 from pydantic import BaseModel, EmailStr
+from fastapi.responses import JSONResponse
 from typing import List, Optional, Union, Dict
+from fastapi import FastAPI, status, Form, UploadFile, File, HTTPException, Request
 
 app = FastAPI()
 
@@ -322,3 +323,30 @@ async def read_http_exception_error_handling_item(item_id: str):
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item not found")
     return {"item": items[item_id]}
+
+# Exception with custom header
+
+@app.get("/assert_items_with_custom_header/{item_id}")
+async def read_item_with_custom_header(item_id: str):
+    if item_id not in items:
+        raise HTTPException(status_code=404,detail="Item not found",headers={"X-Error": "There goes my error"},)
+    return {"item": items[item_id]}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+
+@app.get("/unicorns_exception_handler/{name}")
+async def read_unicorn_exception_handler(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_exception_name": name}
