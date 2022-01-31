fd4ba22a90005a1ae321e97d46dd50ca37fc7008
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-31 05:39:22 -0600

    Update dependencies_injection.py

--- FastAPI/main/dependencies_injection.py
+++ FastAPI/main/dependencies_injection.py
@@ -34,3 +34,50 @@ async def read_items(commons: dict = Depends(common_parameters)):
 @app.get("/dependency_query_users/", tags=["Dependecies injection"])
 async def read_users(commons: dict = Depends(common_parameters)):
     return commons
+
+# Classes as dependencies
+
+class Cat:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def __str__(self):
+        return self
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, query: Optional[str] = None, skip: int = 0, limit: int = 100):
+        self.query = query
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/common_query_parameters_items/", tags=["Class Common Query Parameters Dependencies"])
+async def read_common_query_parameters_items(commons: CommonQueryParams = Depends(CommonQueryParams)):
+    response = {}
+    if commons.query:
+        response.update({"query_string": commons.query})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"query_items": items})
+    return response
+
+@app.get("/common_query_parameters_without_type_annonation_items/", tags=["Class Common Query Parameters Dependencies"])
+async def read_common_query_parameters_without_type_annonation_items(commons = Depends(CommonQueryParams)):
+    response = {}
+    if commons.query:
+        response.update({"query_string": commons.query})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"query_items": items})
+    return response
+
+@app.get("/common_query_parameters_with_shortcut_method_items/", tags=["Class Common Query Parameters Dependencies"])
+async def read_common_query_parameters_with_shortcut_mothod_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.query:
+        response.update({"query_string": commons.query})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"iquery_tems": items})
+    return response
