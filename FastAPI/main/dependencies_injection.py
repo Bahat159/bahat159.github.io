b7a9ab4622342320f119a848b3c3dd697f9810a7
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-01-31 05:21:40 -0600

    Update dependencies_injection.py

--- FastAPI/main/dependencies_injection.py
+++ FastAPI/main/dependencies_injection.py
@@ -11,7 +11,13 @@ from fastapi import Depends, FastAPI
 # And then, that system (in this case FastAPI) will 
 # take care of doing whatever is needed to provide your code 
 # with those needed dependencies ("inject" the dependencies).
-
+#
+# Other common terms for this same idea of "dependency injection" are:
+# resources
+# providers
+# services
+# injectables
+# components
 
 app = FastAPI()
 
@@ -20,11 +26,11 @@ async def common_parameters(query_string: Optional[str] = None, optional_query_p
     return {"query_string": query_string, "query_parameter_skip": optional_query_parameter_skip, "query_parameter_limit": optional_query_parameter_limit}
 
 
-@app.get("/items/", tags=["Dependecies injection"])
+@app.get("/dependency_query_items/", tags=["Dependecies injection"])
 async def read_items(commons: dict = Depends(common_parameters)):
     return commons
 
 
-@app.get("/users/", tags=["Dependecies injection"])
+@app.get("/dependency_query_users/", tags=["Dependecies injection"])
 async def read_users(commons: dict = Depends(common_parameters)):
     return commons
