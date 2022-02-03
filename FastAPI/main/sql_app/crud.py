c11699408e6b21ec0eb49808606be0c01d43ea73
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-03 05:52:20 -0600

    Update crud.py

--- FastAPI/main/sql_app/crud.py
+++ FastAPI/main/sql_app/crud.py
@@ -4,7 +4,12 @@ import models, schemas
 
 # CRUD utils
 # CRUD comes from: Create, Read, Update, and Delete.
+#
+# Create utility functions to:
 
+
+
+# Read a single user by ID and by email.
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
@@ -12,10 +17,16 @@ def get_user(db: Session, user_id: int):
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-
+# Read multiple users.
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
+# Create User data
+# 
+# Create a SQLAlchemy model instance with your data.
+# add that instance object to your database session.
+# commit the changes to the database (so that they are saved).
+# refresh your instance (so that it contains any new data from the database, like the generated ID).
 
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
@@ -25,11 +36,11 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.refresh(db_user)
     return db_user
 
-
+# Read multiple items
 def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
-
+# Create User Item data
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db_item = models.Item(**item.dict(), owner_id=user_id)
     db.add(db_item)
