693af909a29321fab68a57695100683dad642204
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-03 03:17:23 -0600

    Create relational_database.py

---
+++ FastAPI/main/relational_database.py
@@ -0,0 +1,6 @@
+# SQL (Relational) Databases
+#
+# ORMs
+#
+# A common pattern is to use an "ORM": an "object-relational mapping" library.
+# An ORM has tools to convert ("map") between objects in code and database tables ("relations").
