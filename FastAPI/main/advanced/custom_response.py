03a03f50af8f830fc4d26cbf7dbcc6bccfeccb38
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-06 05:08:08 -0600

    Update custom_response.py

--- FastAPI/main/advanced/custom_response.py
+++ FastAPI/main/advanced/custom_response.py
@@ -32,3 +32,23 @@ async def return_html_response_items():
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
+
+# Document in OpenAPI and override Response
+
+def generate_html_response():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.get("/read_html_generated_response_items/", response_class=HTMLResponse, tags=["OpenApi and override response"])
+async def read_generated_html_respose_items():
+    return generate_html_response()
