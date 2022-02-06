88393d01b020b1e490487e7028a6ea92cd7af9c6
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-06 05:15:49 -0600

    Update custom_response.py

--- FastAPI/main/advanced/custom_response.py
+++ FastAPI/main/advanced/custom_response.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, Response, PlainTextResponse, UJSONResponse
 
 app = FastAPI()
 
@@ -52,3 +52,37 @@ def generate_html_response():
 @app.get("/read_html_generated_response_items/", response_class=HTMLResponse, tags=["OpenApi and override response"])
 async def read_generated_html_respose_items():
     return generate_html_response()
+
+
+# Response
+#
+# It accepts the following parameters:
+# content - A str or bytes.
+# status_code - An int HTTP status code.
+# headers - A dict of strings.
+# media_type - A str giving the media type. E.g. "text/html".
+
+@app.get("/legacy_data_item/", tags=["Xml Response Type"])
+def get_legacy_data():
+    data = """<?xml version="1.0"?>
+    <shampoo>
+    <Header>
+        Apply shampoo here.
+    </Header>
+    <Body>
+        You'll have to use soap here.
+    </Body>
+    </shampoo>
+    """
+    return Response(content=data, media_type="application/xml", status_code=200)
+
+
+# PlainTextResponse
+@app.get("/plaintext_response_type_item", response_class=PlainTextResponse, tags=["Plaintext Response"])
+async def main_plaintext_response():
+    return "Hello World, Plaintext response from FastApi"
+
+# UJSONResponse
+@app.get("/read_ujson_response_items/", response_class=UJSONResponse, tags=["UJson Response"])
+async def read_ujson_response_items():
+    return [{"item_id": "UJson Response Item"}]
