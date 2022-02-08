008291a7fb5f6ab16181d5647f2637b10b23fa36
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-08 08:20:52 -0600

    Update main.py

--- flask/main.py
+++ flask/main.py
@@ -82,3 +82,35 @@ def login_http_request():
         return do_the_login()
     else:
         return show_the_login_form()
+
+# Static Files
+#
+# Dynamic web applications also need static files. 
+# That’s usually where the CSS and JavaScript files are coming from. 
+# Ideally your web server is configured to serve them for you, but during development 
+# Flask can do that as well. Just create a folder called static in your package or next 
+# to your module and it will be available at /static on the application.
+#
+# To generate URLs for static files, use the special 'static' endpoint name:
+
+# url_for('static', filename='style.css')
+
+
+# Rendering Templates
+# 
+# Generating HTML from within Python is not fun, and 
+# actually pretty cumbersome because you have to do the HTML 
+# escaping on your own to keep the application secure. 
+# Because of that Flask configures the Jinja2 template engine for you automatically.
+#
+# To render a template you can use the render_template() method. 
+# All you have to do is provide the name of the template and the 
+# variables you want to pass to the template engine as keyword arguments. 
+# Here’s a simple example of how to render a template:
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_template(name=None):
+    return render_template('hello.html', name=name)
