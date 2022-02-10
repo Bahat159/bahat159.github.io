d041932d2c0cfb24cdd4b0d664596ce31b3d775a
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-10 01:22:18 -0600

    Create size_test.s

---
+++ C-ASM/assembly/size_test.s
@@ -0,0 +1,10 @@
+# sizetest.s – A sample program to view the executable size
+
+.section .bss
+    .lcomm buffer, 10000
+.section .text
+.globl _start
+_start:
+    movl $1, %eax
+    movl $0, %ebx
+    int $0x80
