f454d15924d2a4980b8e1c9e271af36685b80afa
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-10 03:23:55 -0600

    Update utility.c

--- C-ASM/c_code/utility.c
+++ C-ASM/c_code/utility.c
@@ -1,43 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-
-#define LOWER 0 
-#define UPPER 300 
-#define STEP 20 
-#define MAXLINE 1000 /* maximum input line length */
-#define VERTICAL_TAB '\013' /* ASCII vertical tab */
-#define BELL_ '\007' /* ASCII bell character */
-#define VERTICALTAB '\xb' /* ASCII vertical tab in hexadecimal */
-#define BELL_CHAR '\x7' /* ASCII bell character in hexadecimal */
-
-int calculate_fahrenhite();
-int calculate_fahr_with_for_loop();
-int power_function_call_by_value(int base, int n);
-int getline_input(char line[], int maxline);
-void copy_to_from(char to[], char from[]);
-int copy_and_getline_main();
-int string_length(char s[]);
-int convert_to_lower_char(int c);
-int generate_random(void);
-int binary_search(int x, int v[], int n);
-int alphabet_to_integer(char s[]);
-void shell_sort(int v[], int n);
-void reverse_string(char s[]);
-int trim_character(char s[]);
-int string_index(char s[], char t[]);
-double ato_float(char s[]);
-int ato_integer(char s[]);
-void push_to_stack(double f);
-double pop_from_stack(void);
-int get_character(void);
-void unget_character(int c);
-
-
-enum boolean {NO, YES};
-enum escapes { BELL = '\a', BACKSPACE = '\b', TAB = '\t', NEWLINE = '\n', VTAB = '\v', RETURN = '\r' };
-enum months { JAN = 1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC }; /* FEB = 2, MAR = 3, etc. */
+#include "utility.h"
 
 
 int main(){
@@ -276,9 +240,6 @@ int ato_integer(char s[]){
     return (int) ato_float(s);
 }
 
-#define MAXVAL 100 /* maximum depth of val stack */
-int sp = 0; /* next free stack position */
-double val[MAXVAL]; /* value stack */
 
 /* push: push f onto value stack */
 void push_to_stack(double f){
@@ -300,10 +261,6 @@ double pop_from_stack(void){
         return 0.0;
     }
 }
-
-#define BUFSIZE 100
-char buf[BUFSIZE]; /* buffer for ungetch */
-int bufp = 0; /* next free position in buf */
 
 /* get a (possibly pushed-back) character */
 int get_character(void) {
