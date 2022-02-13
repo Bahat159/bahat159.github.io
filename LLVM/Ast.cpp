a2da33eb84afba0a392f385239ebf0cadf6bba32
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-13 09:35:52 -0600

    Update Ast.cpp

--- LLVM/Ast.cpp
+++ LLVM/Ast.cpp
@@ -64,3 +64,24 @@ class FunctionASt {
     FunctionASt(Std::unique_ptr<PrototypeASt> Proto, std::unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
 
+
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+
+static int Curtok;
+static int getNextToken() {
+    return Curtok = gettok();
+}
+
+/// LogError* - These are little helper functions for error handling.
+
+std::unique_ptr<ExprAST> LogError(const char *Str) {
+    fprintf(stderr, "LogError: %s\n", Str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeASt> LogError(const char *Str) {
+    LogError(Str);
+    return nullptr;
+}
