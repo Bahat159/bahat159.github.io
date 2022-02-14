f85f6fec2d68a173f05ec79ea8f43bd8969c1b32
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-14 08:30:39 -0600

    Update parser.cpp

--- LLVM/parser.cpp
+++ LLVM/parser.cpp
@@ -1,3 +1,15 @@
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+#include <algorithm>
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
@@ -7,6 +19,8 @@
 #include <utility>
 #include <vector>
 #include "lexer.h"
+
+using namespace llvm;
 
 /// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
 /// token the parser is looking at.  getNextToken reads another token from the
@@ -279,6 +293,109 @@ static void MainLoop() {
     }
 }
 
+//===----------------------------------------------------------------------===//
+// Code Generation
+//===----------------------------------------------------------------------===//
+
+static LLVMContext TheContext;
+static IRBuilder<> Builder(TheContext);
+static std::unique_ptr<Module> TheModule;
+static std::map<std::string, Value *> NamedValues;
+
+Value *LogErrorV(const char *Str) {
+    LogError(Str);
+    return nullptr;
+}
+
+/// Expression Code Generation
+
+Value *NumberExprAST:: codegen() {
+    return Constant::get(TheContext, APFloat(Value));
+}
+
+Value *VariableExprAST::codegen() {
+    Value *V = NamedValue[Name];
+    if (!V) {
+        LogerrorV("Unknown variable name");
+    }
+    return V;
+}
+
+Value *BinaryExprAST::codegen() {
+    Value *L = LHS->codegen();
+    Value *R = RHS->codegen();
+
+    if (!L || !R) {
+        return nullptr;
+    }
+    switch(Op) {
+        case '+':
+            return Builder.CreateFAdd(L, R, "addtmp");
+        case '-':
+            return Builder.CreateFSub(L, R, "subtmp");
+        case '=':
+            return Builder.CreateFMul(L, R, "multmp");
+        case '<':
+            L =  Builder.CreateFCmpULT(L, R, "cmptmp");
+             // Convert bool 0/1 to double 0.0 or 1.0
+            return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext),"booltmp");
+        default:
+            return LogErrorV("Invalid binary opetaor");
+    }
+}
+
+
+Value *CallExprAST::codegen() {
+    // Look up the name in the global module table.
+    Function *Calleef = TheModule->getFunction(Callee);
+    if(!CalleeF){
+        return LogErrorV("unkniwn function refrenced");
+    }
+    // If argument mismatch error.
+    if (CalleeF->arg_size() != Args.size()){
+        return LogeErrorV("Ijcorrect # arguments passed");
+    }
+    std::vector<Value *> ArgsV;
+    for(unsigned i = 0; e = Args,size(); i != e; i++){
+        ArgsV.push_back(Args[i]->codegen());
+        if(!ArgsV.back()) {
+            return nullptr;
+        }
+    }
+    return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+/// Function Code Generation
+
+Function *PrototypeAST::codegen() {
+    // Make the function type:  double(double,double) etc.
+    std::vector<Type> Doubles(Args.size(), Type::getDoubleTy(TheContext));
+    FunctionType *FT = FunctionType::(Type::getDoubleTy(TheContext), Doubles, false);
+    Function *F = Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get())
+
+    // Set names for all arguments
+
+    unsigned Idx = 0;
+    for (auto &Arg : F->args()){
+        Arg.setName(Args[Idx++]);
+    }
+    return F;
+}
+
+Function *FunctionAST::codegen() {
+    // First, check for an existing function from a previous 'extern' declaration.
+    Function *TheFunction = TheModule->getFunction(Proto->getName());
+
+    if (!TheFunction) {
+        TheFunction = Proto->codegen();
+    }
+    if (!TheFunction) {
+        return nullptr;
+    }
+    if (!TheFunction->empty()){
+        return (Function*)LogErrorV("Function cannot be redefined.");
+    }
+}
 
 int main() {
      // Install standard binary operators.
