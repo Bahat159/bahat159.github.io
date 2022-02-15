bbd9c845d42df415834231a2726d9fb2b0941c77
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-15 10:41:37 -0600

    Update test.h

--- C-ASM/Cpp/test.h
+++ C-ASM/Cpp/test.h
@@ -2,12 +2,15 @@
 
 char check_type();
 
+#ifndef STRUCT_S
+#define STRUCT_S
 struct S {
     std::string name;
     float num;
     S(std::string s, float f) : name(s), num(f) {}
 };
 
+#endif
 
 #ifndef ACCOUNT_CLASS
 #define ACCOUNT_CLASS
