8a1455dd7366feb3cb05f76a904e0eac7f674345
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-15 07:32:14 -0600

    Update utility.c

--- C-ASM/c_code/utility.c
+++ C-ASM/c_code/utility.c
@@ -642,3 +642,46 @@ int binary_search_in_struct(char *word, struct key tab[], int n){
 Warning: And Todo: 
     utility.h:75:48: warning: ‘struct key’ declared inside parameter list will not be visible outside of this definition or declaration  
 */
+
+char *string_duplicate(char *s){
+    char *p;
+    p = (char *) malloc(strlen(s) + 1);
+    if( p != NULL){
+        copy_to_from_using_pointer(p, s);
+    }
+    return p;
+}
+
+struct tnode *talloc(void){
+    return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+
+void treeprint(struct tnode *p){
+    if (p != NULL){
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+
+struct treenode *addtree(struct tnode *p, char *w){
+    int cond;
+
+    if(p == NULL){
+        p = talloc();
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    }
+    else if((cond = strcmp(w, p->word)) == 0){
+        p->count++;
+    }
+    else if(cond < 0){
+        p->left = addtree(p->left, w);
+    }
+    else {
+        p->right = addtree(p->right, w);
+    }
+    return p;
+}
