a83dab6108f17ee0ea9a797c77a6767939fb7bc7
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-16 11:02:16 -0600

    Create statement.cpp

---
+++ C-ASM/Cpp/src/statement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include "statement.h"
+
+//using namespace std;
+
+void test_label(int x) {
+    if (x == 1){
+        goto label1;
+    }
+    goto label2;
+
+    label1:
+        std::cout << "In Label1 "<<std::endl;
+        return;
+    label2:
+        std::cout << "In Label2 "<<std::endl;
+        return;
+}
+
+int main() {
+    test_label(1);
+    test_label(2);
+
+    std::cout <<"============================" <<"\n";
+
+    using namespace Test;
+    using namespace std;
+
+    string s = Func();
+    std::cout << s << std::endl;
+
+    return 0;
+}
+
+
+char *myStrCpy(char *Dest, const char *Source) {
+    char *DestStart = Dest;
+
+    while(*Dest++ = *Source++){
+        ;
+    }
+    return DestStart;
+}
