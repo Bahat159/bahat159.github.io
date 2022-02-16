b7368a8f5a5b1a46dfa8692cff649f479e9f844d
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-15 19:32:05 -0600

    Update test.h

--- C-ASM/Cpp/test.h
+++ C-ASM/Cpp/test.h
@@ -1,4 +1,6 @@
-
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
 
 char check_type();
 
@@ -21,6 +23,97 @@ class Account{
         double GetBalance(){ return balance; }
     private:
         double balance;
+};
+
+#endif
+
+struct x_ {
+    char a;
+    int b;
+    short c;
+    char d;
+}bar[3];
+
+
+
+// Trivial Type
+#ifndef TRIVIAL_STRUCT
+#define TRIVIAL_STRUCT
+
+struct Trivial {
+    int i;
+    private:
+    int j;
+};
+
+struct Trivial2 {
+    int i;
+    Trivial2(int a, int b) : i(a), j(b) {}
+    Trivial2() = default;
+    private:
+    int j;      // Different access control
+};
+
+#endif
+
+// Standard Layout Type
+#ifndef SL_T
+#define SL_T
+
+struct SL {
+    int i;
+    int j;
+    SL(int a, int b) : i(a), j(b) {}  // User-defined constructor OK
+};
+
+struct Base {
+    int i;
+    int j;
+    void Foo() {}
+};
+
+struct Derived : public Base {
+    int x;
+    int y;
+};
+
+#endif
+
+// POD Type
+
+#ifndef PO_D
+#define PO_D
+
+struct B {
+    protected:
+        virtual void Foo() {}
+};
+
+// Neither trivial nor standard layout
+
+struct A : B {
+    int a;
+    int b;
+    void Foo() override {}  // virutal function
+};
+
+// Trivial but not standard - layout
+
+struct C {
+    int a;
+    private:
+    int b;    // Different access control
+};
+
+struct D {
+    int a;
+    int b;
+    D () {}  // User-defined constructor
+};
+
+struct POD {
+    int a;
+    int b;
 };
 
 #endif
