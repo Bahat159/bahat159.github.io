007d9dcb86c713e08f5dfb062341bcd0e48fd090
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-16 07:44:41 -0600

    Update expression.h

--- C-ASM/Cpp/include/expression.h
+++ C-ASM/Cpp/include/expression.h
@@ -1,44 +1,107 @@
 #include <iostream>
-#include <typeinfo>
-#include <iostream>
+#include "expression.h"
+
+using namespace std;
 
 
-struct Alpha {
-    virtual void test() {
-        std::cout <<"In Alpha" <<"\n";
+void print(const char *string, const char *terminator = "\n");
+
+
+void printt() {
+    std::cout << endl;
+}
+
+template <typename T> void printt(const T& t) {
+    std::cout << t << endl;
+}
+
+template <typename First, typename... Rest> void printt(const First& first, const Rest&... rest) {
+    std::cout << first <<", ";
+    printt(rest...);
+}
+
+
+int main() {
+    print("hello, ");
+    print("world!");
+
+    print("good morning", ", ");
+    print("sunshine.");
+
+    std::cout<<"======================================"<<"\n";
+
+    printt();
+    printt(1);
+
+    printt(10, 20);
+    printt(100, 200, 300);
+    printt("first", 2, "third", 3.14159);
+
+    std::cout<<"======================================"<<"\n";
+
+    Alpha *pa = new Calm;
+    Alpha *pa2 = new Beta;
+
+    pa->test();
+
+    Beta * pb = dynamic_cast<Beta *>(pa);
+    if(pb) {
+        pb->test2();
     }
-};
-
-struct Beta : Alpha {
-    virtual void test() {
-        std::cout << "In Beta" <<"\n";
+    Calm * pc = dynamic_cast<Calm *>(pa2);
+    if(pc) {
+        pc->test2();
     }
-    void test2() {
-        std::cout << "Test2 in Beta" <<"\n";
+    Calm ConStack;
+    Globaltest(ConStack);
+
+    Beta BonStack;
+    Globaltest(BonStack);
+    std::cout<<"======================================"<<"\n";
+
+    Shape shape_instance;
+    Shape& ref_shape = shape_instance;
+    try{
+        Circle& ref_circle = dynamic_cast<Circle&> (ref_shape);
     }
-};
-
-struct Calm : Beta {
-    virtual void test() {
-        std::cout << "In Calm" <<"\n";
+    catch(bad_cast b){
+        std::cout << "Caught: "<< b.what() <<"\n";
     }
 
-    void test2(){
-        std::cout<<"Test2 in Calm" <<"\n";
+    std::cout<<"======================================"<<"\n";
+
+    CCTest X;
+    X.setNumber(8);
+    X.printNumber();
+
+    std::cout<<"======================================"<<"\n";
+
+}
+
+void print(const char *string, const char *terminator) {
+    if(string != NULL) {
+        std::cout << string;
     }
-};
+    if(terminator != NULL) {
+        std::cout << terminator;
+    }
+}
+
+void Globaltest(Alpha& a) {
+    try{
+        Calm &c = dynamic_cast<Calm&> (a);
+        std::cout << "In GlobalTest"<< "\n";
+    }
+    catch(std::bad_cast) {
+        std::cout <<"Cant cast to Calm" <<"\n";
+    }
+}
 
 
-void Globaltest(Alpha& a);
+void CCTest::setNumber(int num) { number = num ;}
 
-
-class Shape {
-    public:
-        virtual void virtualfunc () const {}
-};
-
-class Circle : public Shape {
-    public:
-        virtual void virtualfunc() const {}
-};
-
+void CCTest::printNumber() const {
+    std::cout << "Before: "<< number << "\n";
+    const_cast<CCTest *> (this)->number--;
+    std::cout << "After: " << number << "\n";
+}
