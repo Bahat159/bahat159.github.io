01248da2ffc92c3544ea724a038901413dda0fb2
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-02-16 12:50:54 -0600

    Create functions.h

---
+++ C-ASM/Cpp/include/functions.h
@@ -0,0 +1,60 @@
+#include <type_traits>
+#include <string>
+#include <tuple>
+#include <stdarg.h>
+
+int sum(int a, int b);
+int foo(int i, std::string s);
+constexpr float exp(float x, int n);
+
+#if ECS == gnu
+#include <cstring>
+#define ECString string
+using namespace std;
+#else
+#include <bstring.h>
+#define ECString string
+#endif
+
+#pragma GCC system_header
+
+/*
+//Declare printf with C linkage.
+extern "C" int printf( const char *fmt, ... );
+
+inline double Account::GetBalance();
+
+template <typename T> T copy_object(T& obj) noexcept(std::is_pod<T>);
+template<typename Lhs, typename Rhs> auto Add2(const Lhs& lhs, const Rhs& rhs);
+
+// Default Arguments 
+int DoSomething(int num,string str,Allocator& alloc = defaultAllocator);
+int DoSomethingElse(int num, string str = string{ "Working" }, Allocator& alloc = defaultAllocator);
+
+//Trailing return types 
+
+template<typename Lhs, typename Rhs> auto Add(const Lhs& lhs, const Rhs& rhs) -> decltype(lhs + rhs);
+//Type deduction in return types (C++14) 
+template<typename F, typename Tuple = tuple<T...>, int... I> decltype(auto) apply_(F&& f, Tuple&& args, index_sequence<I...>);
+template<typename F, typename Tuple = tuple<T...>,typename Indices = make_index_sequence<tuple_size<Tuple>::value >>decltype( auto) \
+ apply(F&& f, Tuple&& args);
+
+*/
+struct Simp {
+    string name;
+    int num;
+};
+
+Simp ggg();
+
+
+tuple<int, string, double>f();
+
+struct Subt {
+    string name;
+    int  num;
+};
+
+Subt g();
+
+void ShowVar( char *szTypes, ... );
