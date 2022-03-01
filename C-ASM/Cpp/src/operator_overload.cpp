d01a58ca32f7facace2310638a530a5432d1ba8e
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-01 10:20:36 -0600

    Update operator_overload.cpp

--- C-ASM/Cpp/src/operator_overload.cpp
+++ C-ASM/Cpp/src/operator_overload.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "operator_overload.h"
+#include "operator_overloading.h"
 
 int main() {
     Complex a = Complex(1.2, 3.4);
@@ -9,5 +9,30 @@ int main() {
     c = a + b;
     c.Display();
 
+    std::cout << "================================"<<"\n";
+
+    Int i;
+    i.operator++(25);
+
+    Point_x pt1, pt2;
+    pt1 = pt2;
+
     return 0;
+}
+
+Int Int::operator++(int n) {
+    Int result = *this;
+    if (n != 0) {
+        _i += n;
+    }
+    else {
+        _i++;
+        return result;
+    }
+}
+
+Point_x& Point_x::operator=(const Point_x& otherPoint) {
+    _x = otherPoint._x;
+    _y = otherPoint._y;
+    return *this;
 }
