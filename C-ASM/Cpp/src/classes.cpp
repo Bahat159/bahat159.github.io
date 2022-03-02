5e91430030dfaecbc574ffc51bf882229b02e40b
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-02 09:40:24 -0600

    Update classes.cpp

--- C-ASM/Cpp/src/classes.cpp
+++ C-ASM/Cpp/src/classes.cpp
@@ -7,7 +7,23 @@ int main() {
     labrador.setEars("long", "floppy");
     std::cout << "Cody is a "<< labrador.getColor() << " Labrador" <<std::endl;
 
-    
+    std::cout <<"======================================="<<std::endl;
+
+    DerivedClass1 derived_class1;
+    DerivedClass2 derived_class2;
+
+    /* derived_class1.PublicFunc();
+    derived_class2.PublicFunc(); */       // function is inaccessible
+
+    VFuncDerived vfd;
+    VFuncBase *pvfb = &vfd;
+    VFuncDerived *pvdf = &vfd;
+    int State;
+
+    /*
+    State = pvdf->GetState(); 
+    State = pvdf->GetState();
+    */
 
     return 0;
 }
