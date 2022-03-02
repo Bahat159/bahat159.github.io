b63d25d190f1b2fe47a0bd6db032ec8f96e576f9
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-02 09:05:25 -0600

    Update classes.h

--- C-ASM/Cpp/include/classes.h
+++ C-ASM/Cpp/include/classes.h
@@ -7,31 +7,54 @@ class dog {
             _legs = 4;
             _bark = true;
         }
-        void setDogSize(string dogSize) {
+        void setDogSize(std::string dogSize) {
             _dogSize = dogSize;
         }
-        virtual void setEars(string type) {
+        virtual void setEars(std::string type) {
             _earType = type;
         }
     private:
-        string _dogSize, _earType;
+        std::string _dogSize, _earType;
         int _legs;
         bool _bark;
 };
 
 class breed : public dog {
     public:
-        breed(string color, string size) {
+        breed(std::string color, std::string size) {
             _color = color;
             setDogSize(size);
         }
-        string getColor() {
+        std::string getColor() {
             return _color;
         }
-        void setEars(string length, string type) {
+        void setEars(std::string length, std::string type) {
             _earLength = length;
             _earType = type;
         }
     protected:
-        string _color, _earLength, _earType;
+        std::string _color, _earLength, _earType;
 };
+
+
+class TestRun {
+    public:
+        TestRun() = default;
+        TestRun(const TestRun&) = delete;
+        TestRun(std::string name);
+        void DoSomething();
+        int Calculate(int a, double b);
+        virtual ~TestRun();
+        enum class State { Active, Suspended};
+    protected:
+        virtual void Initiailize();
+        virtual void Suspend();
+        State GetState();
+    private:
+        State _state{ State::Suspended};
+        std::string _testName{ ""};
+        int _index{ 0 };
+        static int _instances;
+};
+
+int TestRun::_instances { 0 };
