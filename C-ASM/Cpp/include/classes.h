f36e19366cc4312273fa099a237e1fa4c31c5fc4
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-05 14:54:57 -0600

    Update classes.h

--- C-ASM/Cpp/include/classes.h
+++ C-ASM/Cpp/include/classes.h
@@ -438,3 +438,40 @@ class Dervied_Class : public BaseClass1, public BaseClass2, public BaseClass3 {
     public:
         Dervied_Class() {std::cout <<"Dervied_Class ctor" <<std::endl; }
 };
+
+
+class MemoryBlock {
+    public:
+        explicit MemoryBlock(size_t length) : _length(length), _data(new int[length]) {
+            std::cout <<"In MemoryBlock(size_t). length = "<<_length<<"."<<std::endl;
+        }
+        ~MemoryBlock() {
+            std::cout<<"In ~MemoryBlock(). length = "<<_length<<"."<<std::endl;
+            if(_data != nullptr) {
+                std::cout<<"Deleting resource.";
+                delete[] _data;
+            }
+            std::cout<< std::endl;
+        }
+        MemoryBlock(const MemoryBlock& other) : _length(other._length), _data(new int[other._length]) {
+            std::cout<<"In MempryBlock(const MemoryBlock&).length = "<<other._length<<". Copying resources."<<std::endl;
+            std::copy(other._data, other._data+_length,_data);
+        }
+        MemoryBlock& operator=(const MemoryBlock& other) {
+            std::cout<<"In operator=(const MemoryBlock& other).length = "<<other._length<<". Copying resource."<<std::endl;
+            if(this != &other){
+                delete[] _data;
+
+                _length = other._length;
+                _data = new int[_length];
+                std::copy(other._data, other._data+_length, _data);
+            }
+            return *this;
+        }
+        size_t Length() const {
+            return _length;
+        }
+    private:
+        size_t _length;
+        int* _data;
+};
