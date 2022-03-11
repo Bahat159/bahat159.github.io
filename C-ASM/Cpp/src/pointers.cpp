2b2b34070241f0eff4fc95823d9984ee5a1bb09b
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-11 01:14:43 -0600

    Update pointers.cpp

--- C-ASM/Cpp/src/pointers.cpp
+++ C-ASM/Cpp/src/pointers.cpp
@@ -1,25 +1,8 @@
 #include <iostream>
 #include <string>
+#include "pointers.h"
 
 
-class MyClass {
-    public:
-        int num;
-        std::string name;
-        void print() {
-            std::cout<<name<<" : "<<num <<std::endl;
-        }
-};
-
-void func_A(MyClass* mc) {
-    mc->num = 3;
-}
-
-void func_B(MyClass mc) {
-    mc.num = 21;
-    std::cout <<"Local copy of mc: ";
-    mc.print();
-}
  
 void func(int arr[], int length);
 
@@ -50,12 +33,9 @@ int main() {
     int i[5]{2,4,6,8,10};
     int j = sizeof(i) / sizeof(i[0]);
     func(i,j);
+
+    std::cout<<"=================================" <<std::endl;
+
+    
 }
 
-
-void func(int *arr, int length) {
-    size_t test = sizeof(arr);
-    for(int i = 0; i < length; ++i) {
-        std::cout<<arr[i] << " "<<std::endl;
-    }
-}
