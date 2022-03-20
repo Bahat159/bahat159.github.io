b0353db12fd99169e6e34f43f868b2fcc286086f
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-20 08:23:18 -0500

    Update template.cpp

--- C-ASM/Cpp/src/template.cpp
+++ C-ASM/Cpp/src/template.cpp
@@ -7,5 +7,9 @@ int main() {
     int b = 10;
     int i = minimum_t<int>(a, b);
     std::cout<<"Minimum returned: "<<i<<std::endl;
+
+    MyClass mc1 { 1, L"Hello"};
+    MyClass mc2 { 2, L"goodbye"};
+    
     return 0;
 }
