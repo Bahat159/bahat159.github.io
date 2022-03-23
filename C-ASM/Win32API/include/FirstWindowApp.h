fd4f146ef293b7d15b8ff00c7be596121e6b565b
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-23 09:55:36 -0500

    Update FirstWindowApp.h

--- C-ASM/Win32API/include/FirstWindowApp.h
+++ C-ASM/Win32API/include/FirstWindowApp.h
@@ -71,9 +71,92 @@ protected:
 	HWND m_hwnd;
 };
 
+
+template <class T> void SafeRelease(T** ppT)
+{
+	if (*ppT) {
+		(*ppT)->Release();
+		*ppT = NULL;
+	}
+}
+
 class MainWindow : public BaseWindow<MainWindow>
 {
+	ID2D1Factory *pFactory;
+	ID2D1HwndRenderTarget *pRenderTarget;
+	ID2D1SolidColorBrush *pBrush;
+	D2D1_ELLIPSE            ellipse;
+
+	void CalculateLayout();
+	HRESULT CreateGraphicsResources();
+	void DiscardGraphicsResources();
+	void OnPaint();
+	void Resize();
 public:
-	PCWSTR ClasName() const { return L"Sample Window Class"; }
+	MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL) {}
+	PCWSTR ClassName() const {
+		return L"Circle Window Class";
+	}
 	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
+
+void MainWindow::CalculateLayout() {
+	if (pRenderTarget != NULL) {
+		D2D1_SIZE_F size = pRenderTarget->GetSize();
+		const float x = size.width / 2;
+		const float y = size.height / 2;
+		const float radius = min(x, y);
+		ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+	}
+}
+
+HRESULT MainWindow::CreateGraphicsResources() {
+	HRESULT hr = S_OK;
+	if (pRenderTarget == NULL) {
+		RECT rc;   
+		GetClientRect(m_hwnd, &rc);
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+		hr = pFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(m_hwnd, size), &pRenderTarget);
+		if (SUCCEEDED(hr)) {
+			const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
+			hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+			if (SUCCEEDED(hr)) {
+				CalculateLayout();
+			}
+		}
+	}
+	return hr;
+}
+
+void MainWindow::DiscardGraphicsResources() {
+	SafeRelease(&pRenderTarget);
+	SafeRelease(&pBrush);
+}
+
+void MainWindow::OnPaint() {
+	HRESULT hr = CreateGraphicsResources();
+	if (SUCCEEDED(hr)) {
+		PAINTSTRUCT ps;
+		BeginPaint(m_hwnd, &ps);
+		pRenderTarget->BeginDraw();
+		pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+		pRenderTarget->FillEllipse(ellipse, pBrush);
+		hr = pRenderTarget->EndDraw();
+		if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET) {
+			DiscardGraphicsResources();
+		}
+		EndPaint(m_hwnd, &ps);
+
+	}
+}
+
+void MainWindow::Resize() {
+	if (pRenderTarget != NULL) {
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+		pRenderTarget->Resize(size);
+		CalculateLayout();
+		InvalidateRect(m_hwnd, NULL, FALSE);
+	}
+}
