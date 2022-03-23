c49ee998622d73a8365c252ff171d8f8d37a7376
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-23 07:14:14 -0500

    Create FirstWindowApp.cpp

---
+++ C-ASM/Win32API/src/FirstWindowApp.cpp
@@ -0,0 +1,58 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+#include <windef.h>
+#include <WinBase.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowExW(0, CLASS_NAME, L"First Win32API Application", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL);
+
+	if (hwnd == NULL) {
+		return 0;
+	}
+	ShowWindow(hwnd, nShowCmd);
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+			EndPaint(hwnd, &ps);
+		}
+		return 0;
+		case WM_CLOSE:
+			if (MessageBox(hwnd, L"Really quit?", L"First Win32API Application", MB_OKCANCEL) == IDOK) {
+				DestroyWindow(hwnd);
+			}
+			return 0;
+	}
+	return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+}
+
