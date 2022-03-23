9fa5d62672e7be0d715ec65bd06bbec57697f067
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-03-23 07:14:41 -0500

    Create file_dialog.cpp

---
+++ C-ASM/Win32API/src/file_dialog.cpp
@@ -0,0 +1,29 @@
+#include <windows.h>
+#include <shobjidl.h>
+
+DWORD Get_FileDialog() {
+	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+	if (SUCCEEDED(hr)) {
+		IFileOpenDialog *pFileOpen;
+		hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+		if (SUCCEEDED(hr)) {
+			hr = pFileOpen->Show(NULL);
+			if (SUCCEEDED(hr)) {
+				IShellItem* pItem;
+				hr = pFileOpen->GetResult(&pItem);
+				if (SUCCEEDED(hr)) {
+					PWSTR pszFilePath;
+					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+					if (SUCCEEDED(hr)) {
+						MessageBoxW(NULL, pszFilePath, L"FIle Path", MB_OK);
+						CoTaskMemFree(pszFilePath);
+					}
+					pItem->Release();
+				}
+			}
+			pFileOpen->Release();
+		}
+		CoUninitialize();
+	}
+	return 0;
+}
