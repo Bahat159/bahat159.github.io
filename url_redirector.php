dfd06a4128351ff698599020b471f6f9478edd23
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2022-04-12 16:06:14 -0500

    Create url_redirector.php

---
+++ url_redirector.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+error_reporting(-1);
+?>
+<html>
+    <title>Redirecting.....</title>
+    <body onload="document.form.submit()">
+		<form name="form" id="form" action="" method="POST">
+		<div style="display:none;">
+		<input name="" value="">
+		<input type="submit" value="Continue">
+		</div>
+		</form>
+	</body>
+</html>
+		
